42a73b687a71c18ec294940c987abc88e0dc4c60
Theodore Quinn <tcquinn@tcquinn.org>
2019-12-03 19:41:25 -0600

    Rename imports in test scripts

--- tests/3d_pose_tracking_example.py
+++ tests/3d_pose_tracking_example.py
@@ -1,5 +1,5 @@
-import cvutilities.openpose_utilities
-import cvutilities.camera_utilities
+import 3d_pose_tracking
+import cv_utils
 import numpy as np
 import pickle
 import pandas as pd
@@ -23,19 +23,19 @@ output_dataframe_path = os.path.join(
 
 room_size = np.array([7.0, 8.0, 3.0])
 
-pose_initialization_model = cvutilities.openpose_utilities.PoseInitializationModel(
+pose_initialization_model = 3d_pose_tracking.PoseInitializationModel(
     initial_keypoint_position_means = np.tile(room_size/2, (18, 1)),
     initial_keypoint_velocity_means = np.zeros((18,3)),
     initial_keypoint_position_error = np.amax(room_size)/2.0,
     initial_keypoint_velocity_error = 2.0)
 
-keypoint_model = cvutilities.openpose_utilities.KeypointModel(
+keypoint_model = 3d_pose_tracking.KeypointModel(
     position_observation_error = 1.0,
     reference_delta_t = 0.1,
     reference_position_transition_error = 0.1,
     reference_velocity_transition_error = 0.1)
 
-pose_tracking_model = cvutilities.openpose_utilities.PoseTrackingModel(
+pose_tracking_model = 3d_pose_tracking.PoseTrackingModel(
     cost_threshold = 1.0,
     num_missed_observations_threshold = 10)
 
@@ -47,21 +47,21 @@ num_rows = input_dataframe.shape[0]
 
 camera_names = input_dataframe.columns.levels[0].tolist()
 
-camera_calibration_parameters = cvutilities.camera_utilities.fetch_camera_calibration_data_from_local_drive_multiple_cameras(
+camera_calibration_parameters = cv_utils.fetch_camera_calibration_data_from_local_drive_multiple_cameras(
     camera_names = camera_names,
     camera_calibration_data_directory = './data')
 
 initial_dataframe_row = input_dataframe.iloc[0]
 
-initial_poses_2d = cvutilities.openpose_utilities.Poses2D.from_dataframe_row(
+initial_poses_2d = 3d_pose_tracking.Poses2D.from_dataframe_row(
     dataframe_row = initial_dataframe_row,
     camera_names = camera_names)
 
-initial_poses_3d = cvutilities.openpose_utilities.Poses3D.from_poses_2d(
+initial_poses_3d = 3d_pose_tracking.Poses3D.from_poses_2d(
     poses_2d = initial_poses_2d,
     cameras = camera_calibration_parameters)
 
-pose_tracks = cvutilities.openpose_utilities.Pose3DTracks.initialize(
+pose_tracks = 3d_pose_tracking.Pose3DTracks.initialize(
     pose_initialization_model = pose_initialization_model,
     keypoint_model = keypoint_model,
     pose_tracking_model = pose_tracking_model,
@@ -70,10 +70,10 @@ pose_tracks = cvutilities.openpose_utilities.Pose3DTracks.initialize(
 start_time = time.time()
 for row_index in range(1, num_rows):
     dataframe_row = input_dataframe.iloc[row_index]
-    poses_2d = cvutilities.openpose_utilities.Poses2D.from_dataframe_row(
+    poses_2d = 3d_pose_tracking.Poses2D.from_dataframe_row(
         dataframe_row,
         camera_names)
-    poses_3d = cvutilities.openpose_utilities.Poses3D.from_poses_2d(
+    poses_3d = 3d_pose_tracking.Poses3D.from_poses_2d(
         poses_2d,
         camera_calibration_parameters)
     pose_tracks.update(poses_3d)
